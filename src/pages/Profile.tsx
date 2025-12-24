c7c982e8a5f47ed0762ad852051a2067170d4154
Jonathan Giovannini <jonathangiovannini@Mac.lan>
2025-12-24 14:14:18 +0100

    Creata pagina profilo

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -14,13 +14,14 @@ const Profile = () => {
                             <img src="images/goku.png" alt="" />
                         </div>
                     </div>
-                    <div className = 'w-11/12 lg:w-3/4'>
-                        <div className = 'fle'>
-
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quos similique, voluptate eligendi aut sapiente assumenda quod non vel obcaecati dolores minus eveniet ea laudantium placeat consectetur veritatis unde alias nemo odit incidunt iste! Similique veritatis sapiente est labore delectus fuga voluptate possimus, tempora quam! Quasi, id sapiente? Aut, natus.</p>
+                    <div className = 'w-11/12 lg:w-3/4 lg:mt-0 mt-8'>
+                        <div className = 'flex flex-col gap-4 justify-center lg:items-start items-center'>
+                            <h2 className = 'font-bold text-3xl'>Username</h2>
+                            <p className = ''>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quos similique, voluptate eligendi aut sapiente assumenda quod non vel obcaecati dolores minus eveniet ea laudantium placeat consectetur veritatis unde alias nemo odit incidunt iste! Similique veritatis sapiente est labore delectus fuga voluptate possimus, tempora quam! Quasi, id sapiente? Aut, natus.</p>
                         </div>
                     </div>
                 </div>
+                <button className = 'w-48 h-12 font-bold'>Modifica Profilo</button>
                 <div className = 'w-11/12 lg:w-3/4'>
                     <StatGrid/>
                 </div>
